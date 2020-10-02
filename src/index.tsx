23de34ef735640b2183dce3719f93c0ad2335ede
Andrew Hu <andrew.hu@pithecine.com>
2020-10-02 16:18:45 -0400

    port(earth): Migrate project to React/Typescript

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => <div>Hello world!</div>;
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
