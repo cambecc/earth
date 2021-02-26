fc2661b29e83d44b1891f19e2e2b10ba2357524f
cortehz <omanchis@gmail.com>
2021-02-26 16:47:09 +0100

    outdated npm packages & import/require statements

--- dev-server.js
+++ dev-server.js
@@ -8,46 +8,53 @@ console.log("============================================================");
 console.log(new Date().toISOString() + " - Starting");
 
 var util = require("util");
+var logger = require("morgan");
 
 /**
  * Returns true if the response should be compressed.
  */
 function compressionFilter(req, res) {
-    return (/json|text|javascript|font/).test(res.getHeader('Content-Type'));
+  return /json|text|javascript|font/.test(res.getHeader("Content-Type"));
 }
 
 /**
  * Adds headers to a response to enable caching.
  */
 function cacheControl() {
-    return function(req, res, next) {
-        res.setHeader("Cache-Control", "public, max-age=300");
-        return next();
-    };
+  return function (req, res, next) {
+    res.setHeader("Cache-Control", "public, max-age=300");
+    return next();
+  };
 }
 
-function logger() {
-    express.logger.token("date", function() {
-        return new Date().toISOString();
-    });
-    express.logger.token("response-all", function(req, res) {
-        return (res._header ? res._header : "").trim();
-    });
-    express.logger.token("request-all", function(req, res) {
-        return util.inspect(req.headers);
-    });
-    return express.logger(
-        ':date - info: :remote-addr :req[cf-connecting-ip] :req[cf-ipcountry] :method :url HTTP/:http-version ' +
-        '":user-agent" :referrer :req[cf-ray] :req[accept-encoding]\\n:request-all\\n\\n:response-all\\n');
+function appLogger() {
+  logger.token("date", function () {
+    return new Date().toISOString();
+  });
+
+  logger.token("response-all", function (req, res) {
+    return (res._header ? res._header : "").trim();
+  });
+
+  logger.token("request-all", function (req, res) {
+    return util.inspect(req.headers);
+  });
+  return logger(
+    ":date - info: :remote-addr :req[cf-connecting-ip] :req[cf-ipcountry] :method :url HTTP/:http-version " +
+      '":user-agent" :referrer :req[cf-ray] :req[accept-encoding]\\n:request-all\\n\\n:response-all\\n'
+  );
 }
 
 var port = process.argv[2];
 var express = require("express");
+var compression = require("compression");
 var app = express();
 
 app.use(cacheControl());
-app.use(express.compress({filter: compressionFilter}));
-app.use(logger());
+
+app.use(compression({ filter: compressionFilter }));
+// app.use(express.compress({filter: compressionFilter}));
+app.use(appLogger());
 app.use(express.static("public"));
 
 app.listen(port);
